12e50e2567108b7cf939319623924ced77d6cbe8
Jerry Price <jerryprice@lmo.com>
2016-05-21 22:51:26 -0400

    change type

---
+++ src/Composer/Installers/ExpressEngineInstaller.php
@@ -0,0 +1,14 @@
+<?php
+namespace Composer\Installers;
+
+class ExpressionEngineInstaller extends BaseInstaller
+{
+
+    protected $system = 'system/';
+    protected $webroot = 'public/';
+
+    protected $locations = array(
+        'addon'   => '{$this->system}user/addons/{$name}/',
+        'theme'   => '{$this->webroot}themes/user/{$name}/',
+    );
+}
